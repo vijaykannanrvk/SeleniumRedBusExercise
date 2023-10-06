cb000e5b0bdf2d0c70831810858a65f266b81def
vijaykannan <vijaykannnan1992@gmail.com>
2023-10-06 16:09:31 +0530

    Add project files.

---
+++ SeleniumRedBusExercise/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumRedBusExercise")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumRedBusExercise")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba07c911-708d-4cd5-b5d2-bd1d4c7b7154")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumRedBusExercise/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Threading;
+
+namespace SeleniumRedBusExercise
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ChromeOptions chrome_options = new ChromeOptions();
+            chrome_options.AddArgument("--disable-notifications");
+            ChromeDriver driver = new ChromeDriver(chrome_options);
+            driver.Url = "https://www.redbus.in/";
+            driver.Manage().Timeouts().ImplicitWait =TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.XPath("//label[text()='From']/..//input")).SendKeys("Madurai");
+            driver.FindElement(By.XPath("//label[text()='To']/..//input")).SendKeys("Chennai");
+            driver.FindElement(By.XPath("//div/span[text()='Date']")).Click();
+            driver.FindElement(By.XPath("//div/span[contains(@class,'CalendarDaysSpan') and text()='12']")).Click();
+            Thread.Sleep(2000);
+            IWebElement element=driver.FindElement(By.Id("search_button"));
+            element.Click();
+            Actions action = new Actions(driver);
+            action.MoveToElement(element).Click().Build().Perform();
+            driver.FindElement(By.XPath("//label[contains(@for,'SLEEPER')]")).Click();
+            driver.FindElement(By.XPath("//a[text()='Departure']")).Click();
+            driver.FindElement(By.XPath("//div[text()='View Seats']")).Click();
+        }
+    }
+}
